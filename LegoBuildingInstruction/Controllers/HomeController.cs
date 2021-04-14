610b4811997ece848daf968eff60ae887da17207
Lukasz <lukasz.neumann90@gmail.com>
2021-04-14 14:44:41 +0200

    Home controller

---
+++ LegoBuildingInstruction/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BethanysPieShop.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+          
+
+            return View();
+        }
+    }
+}
